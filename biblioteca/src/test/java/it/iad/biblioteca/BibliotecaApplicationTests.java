89961f3f44afca72d83c45a8c18d85ccf066e960
Angelita Doria <angiegiuse@gmail.com>
2021-05-03 15:20:28 +0200

    Server: primo commit

---
+++ biblioteca/src/test/java/it/iad/biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.iad.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
