22a426d23d116f9417e1cfaa896c40dc13e042c6
Andrea Tangucci <andrea.tangucci@gmail.com>
2021-05-04 10:17:20 +0200

    Creata home page e search

--- biblioteca-client/src/app/app.module.ts
+++ biblioteca-client/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    SearchComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
